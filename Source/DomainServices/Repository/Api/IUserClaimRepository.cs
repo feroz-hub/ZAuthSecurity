9fee88578013a88413307ef4ca4135ed2c2fc841
Feroze basha S <bashaferoz66@gmail.com>
2024-05-01 14:52:26 +0530

    IUserClaimRepository

--- Source/DomainServices/Repository/Api/IUserClaimRepository.cs
+++ Source/DomainServices/Repository/Api/IUserClaimRepository.cs
@@ -1,6 +1,18 @@
+using System.Security.Claims;
+using Domain;
+using Domain.Entities.Api;
+
 namespace DomainServices.Repository.Api;
 
 public interface IUserClaimRepository
 {
-    
+    Task InsertAsync(UserClaims entity);
+    Task UpdateAsync(UserClaims entity);
+    Task DeleteAsync(int id);
+    Task DeleteAsync(IEnumerable<UserClaims> entityList);
+    Task<UserClaims> FindIdByClaimAsync(Guid userId, Claim claim, bool isAdminClaim = false);
+    Task<UserClaims> FindUserClaimByIdAsync(int userClaimId);
+    Task<IReadOnlyList<UserClaims>> GetClaimsAsync(Guid userId);
+    Task<IReadOnlyList<UserClaims>> GetAdminUserClaimsAsync(Guid userId, bool getOnlyAdminClaim = true);
+    Task<FrameworkResult> SaveChangesAsync();
 }
