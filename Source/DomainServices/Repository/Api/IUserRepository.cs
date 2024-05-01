0371365b4036de035235e16a1fdebbb3af591469
feroz-hub <bashaferoz66@gmail.com>
2024-05-01 15:05:03 +0530

    IUserRepository Summary Comment

--- Source/DomainServices/Repository/Api/IUserRepository.cs
+++ Source/DomainServices/Repository/Api/IUserRepository.cs
@@ -3,15 +3,71 @@ using Domain.Entities.Api;
 
 namespace DomainServices.Repository.Api;
 
+/// <summary>
+/// Interface defining operations for managing users in the repository.
+/// </summary>
 public interface IUserRepository
 {
+    /// <summary>
+    /// Deletes a user asynchronously.
+    /// </summary>
+    /// <param name="entity">The user entity to delete.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
     Task DeleteAsync(Users entity);
+
+    /// <summary>
+    /// Updates a user asynchronously, specifying affected properties.
+    /// </summary>
+    /// <param name="entity">The user entity to update.</param>
+    /// <param name="affectedProperties">The properties affected by the update.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
     Task UpdateAsync(Users entity, string[] affectedProperties);
+
+    /// <summary>
+    /// Retrieves users in a specific role asynchronously.
+    /// </summary>
+    /// <param name="roleName">The name of the role.</param>
+    /// <returns>A task representing the asynchronous operation, returning a list of users.</returns>
     Task<IReadOnlyList<Users>> GetUsersInRoleAsync(string roleName);
+
+    /// <summary>
+    /// Sets the added status of an entity asynchronously.
+    /// </summary>
+    /// <typeparam name="T">The type of entity.</typeparam>
+    /// <param name="entity">The entity to set the status for.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
     Task SetAddedStatusAsync<T>(T entity);
+
+    /// <summary>
+    /// Sets the modified status of an entity asynchronously.
+    /// </summary>
+    /// <typeparam name="T">The type of entity.</typeparam>
+    /// <param name="entity">The entity to set the status for.</param>
+    /// <param name="concurrencyStamp">The concurrency stamp for optimistic concurrency.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
     Task SetModifiedStatusAsync<T>(T entity, string concurrencyStamp);
+
+    /// <summary>
+    /// Saves changes made to the repository asynchronously.
+    /// </summary>
+    /// <returns>A task representing the asynchronous operation, returning a framework result.</returns>
     Task<FrameworkResult> SaveChangesAsync();
+
+    /// <summary>
+    /// Enables automatic saving of changes made to identity objects.
+    /// </summary>
+    /// <returns>A task representing the asynchronous operation.</returns>
     Task EnableIdentityAutoSaveChanges();
+
+    /// <summary>
+    /// Disables automatic saving of changes made to identity objects.
+    /// </summary>
+    /// <returns>A task representing the asynchronous operation.</returns>
     Task DisableIdentityAutoSaveChanges();
+
+    /// <summary>
+    /// Retrieves all users asynchronously.
+    /// </summary>
+    /// <returns>A task representing the asynchronous operation, returning a list of users.</returns>
     Task<IReadOnlyList<Users>> GetAllUsersAsync();
 }
--- Source/DomainServices/Repository/Api/IUserRoleRepository.cs
+++ Source/DomainServices/Repository/Api/IUserRoleRepository.cs
@@ -3,13 +3,57 @@ using Domain.Entities.Api;
 
 namespace DomainServices.Repository.Api;
 
+/// <summary>
+/// Interface defining operations for managing user roles in the repository.
+/// </summary>
 public interface IUserRoleRepository
 {
+    /// <summary>
+    /// Inserts a new user role asynchronously.
+    /// </summary>
+    /// <param name="entity">The user role entity to insert.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
     Task InsertAsync(UserRoles entity);
+
+    /// <summary>
+    /// Updates an existing user role asynchronously.
+    /// </summary>
+    /// <param name="entity">The user role entity to update.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
     Task UpdateAsync(UserRoles entity);
+
+    /// <summary>
+    /// Deletes a user role asynchronously.
+    /// </summary>
+    /// <param name="entity">The user role entity to delete.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
     Task DeleteAsync(UserRoles entity);
+
+    /// <summary>
+    /// Deletes multiple user roles asynchronously.
+    /// </summary>
+    /// <param name="entityList">The list of user role entities to delete.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
     Task DeleteAsync(IEnumerable<UserRoles> entityList);
+
+    /// <summary>
+    /// Retrieves a user role asynchronously based on user ID and role ID.
+    /// </summary>
+    /// <param name="userId">The ID of the user.</param>
+    /// <param name="roleId">The ID of the role.</param>
+    /// <returns>A task representing the asynchronous operation, returning the user role.</returns>
     Task<UserRoles> GetUserRoleAsync(Guid userId, Guid roleId);
+
+    /// <summary>
+    /// Retrieves all roles associated with a specific user asynchronously.
+    /// </summary>
+    /// <param name="userId">The ID of the user.</param>
+    /// <returns>A task representing the asynchronous operation, returning a list of user roles.</returns>
     Task<IReadOnlyList<UserRoles>> GetUserRoleAsync(Guid userId);
+
+    /// <summary>
+    /// Saves changes made to the repository asynchronously.
+    /// </summary>
+    /// <returns>A task representing the asynchronous operation, returning a framework result.</returns>
     Task<FrameworkResult> SaveChangesAsync();
 }
