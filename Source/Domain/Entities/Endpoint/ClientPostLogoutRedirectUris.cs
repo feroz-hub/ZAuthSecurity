833965e0e175055498498be96617369a139c1d5d
Feroze basha S <bashaferoz66@gmail.com>
2024-05-01 15:48:12 +0530

    Domain Endpoint Entities

--- Source/Domain/Entities/Endpoint/ClientPostLogoutRedirectUris.cs
+++ Source/Domain/Entities/Endpoint/ClientPostLogoutRedirectUris.cs
@@ -1,6 +1,22 @@
 namespace Domain.Entities.Endpoint;
 
-public class ClientPostLogoutRedirectUris:BaseEntity
+/// <summary>
+/// DB Entity representing ClientPostLogoutRedirectUris table.
+/// </summary>
+public class ClientPostLogoutRedirectUris : BaseEntity
 {
-    
+    /// <summary>
+    /// Gets or sets the Client Identifier.
+    /// </summary>
+    public Guid ClientId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the Post Logout RedirectUri.
+    /// </summary>
+    public string PostLogoutRedirectUri { get; set; }
+
+    /// <summary>
+    /// Gets or sets the Clients.
+    /// </summary>
+    public Clients Client { get; set; }
 }
--- Source/Domain/Entities/Endpoint/ClientRedirectUris.cs
+++ Source/Domain/Entities/Endpoint/ClientRedirectUris.cs
@@ -1,6 +1,22 @@
 namespace Domain.Entities.Endpoint;
 
-public class ClientRedirectUris:BaseEntity
+/// <summary>
+/// DB Entity representing ClientRedirectUris table.
+/// </summary>
+public class ClientRedirectUris : BaseEntity
 {
-    
+    /// <summary>
+    /// Gets or sets the Client Identifier.
+    /// </summary>
+    public Guid ClientId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the RedirectUri.
+    /// </summary>
+    public string RedirectUri { get; set; }
+
+    /// <summary>
+    /// Gets or sets the Clients.
+    /// </summary>
+    public Clients Client { get; set; }
 }
