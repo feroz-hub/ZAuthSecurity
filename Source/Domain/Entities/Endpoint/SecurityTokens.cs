26a526bdca3c3659707916d7d59feb051c4ce655
feroz-hub <bashaferoz66@gmail.com>
2024-05-01 15:52:22 +0530

    Modified Comment for SecurityTokens

--- Source/Domain/Entities/Endpoint/SecurityTokens.cs
+++ Source/Domain/Entities/Endpoint/SecurityTokens.cs
@@ -1,53 +1,52 @@
 namespace Domain.Entities.Endpoint;
 
 /// <summary>
-/// DB Entity representing SecurityTokens table.
+/// Represents a security token entity.
 /// </summary>
 public class SecurityTokens : BaseEntity
 {
     /// <summary>
-    /// Gets or sets the 32 bit random number Key for Security Token.
+    /// Gets or sets the key of the security token.
     /// </summary>
     public virtual string Key { get; set; }
 
     /// <summary>
-    /// Gets or sets the type of Security Token. Currently supported values are Refresh token and Authorization code.
+    /// Gets or sets the type of the token.
     /// </summary>
     public virtual string TokenType { get; set; }
 
     /// <summary>
-    /// Gets or sets the value/data payload of Security Token.
-    /// In Refresh token type, it will be access token raw payload.
+    /// Gets or sets the value of the token.
     /// </summary>
     public virtual string TokenValue { get; set; }
 
     /// <summary>
-    /// Gets or sets the Client identifier for which the Security Token was issued.
+    /// Gets or sets the client ID associated with the token.
     /// </summary>
     public virtual string ClientId { get; set; }
 
     /// <summary>
-    /// Gets or sets the Session identifier for which the Security Token was issued.
+    /// Gets or sets the session ID associated with the token.
     /// </summary>
     public virtual string SessionId { get; set; }
 
     /// <summary>
-    /// Gets or sets the Subject(user) identifier for which the Security Token was issued.
+    /// Gets or sets the subject ID associated with the token.
     /// </summary>
     public virtual string SubjectId { get; set; }
 
     /// <summary>
-    /// Gets or sets the Datetime at which the Security Token was issued.
+    /// Gets or sets the creation time of the token.
     /// </summary>
     public virtual DateTime CreationTime { get; set; }
 
     /// <summary>
-    /// Gets or sets the time(seconds) at which the Security Token expires.
+    /// Gets or sets the expiration time of the token.
     /// </summary>
     public virtual int ExpiresAt { get; set; }
 
     /// <summary>
-    /// Gets or sets the time at which the Security Token was consumed. If its null, it means a new fresh token.
+    /// Gets or sets the time when the token was consumed.
     /// </summary>
     public virtual DateTime ConsumedTime { get; set; }
 }
