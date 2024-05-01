18fd839e8a5b4003320e9cdba23810d7ea969df7
feroz-hub <bashaferoz66@gmail.com>
2024-05-01 17:49:10 +0530

    Modification on AuditTrailMap.cs

--- Source/Infrastructure.Data/Mapper/Api/AuditTrailMap.cs
+++ Source/Infrastructure.Data/Mapper/Api/AuditTrailMap.cs
@@ -6,22 +6,18 @@ namespace Infrastructure.Data.Mapper.Api;
 
 public class AuditTrailMap
 {
-    
     public AuditTrailMap(EntityTypeBuilder<AuditTrail> entityBuilder)
     {
         AuditMapping(entityBuilder);
     }
 
-    
+    private const string AuditTrailTableName = "AuditTrail";
     private static void AuditMapping(EntityTypeBuilder<AuditTrail> entityBuilder)
     {
         if (entityBuilder == null) return;
-        entityBuilder.ToTable("SF_AuditTrail");
-        entityBuilder.HasKey(t => t.Id);
-        entityBuilder.HasIndex(t=> new { t.CreatedOn, t.CreatedBy }).HasDatabaseName("IX_AUD_CRON_CBBY");
-        entityBuilder.HasIndex(t => new { t.CreatedOn, t.ActionType }).HasDatabaseName("IX_AUD_CRON_ACTY");
-        entityBuilder.HasIndex(t => new { t.CreatedBy, t.ActionType }).HasDatabaseName("IX_AUD_CBBY_ACTY");
 
+        entityBuilder.ToTable(AuditTrailTableName);
+        entityBuilder.HasKey(t => t.Id);
         entityBuilder.Property(t => t.ActionType).IsRequired().HasMaxLength(50);
         entityBuilder.Property(t => t.TableName).HasMaxLength(255);
         entityBuilder.Property(t => t.AffectedColumn);
@@ -30,9 +26,13 @@ public class AuditTrailMap
         entityBuilder.Property(t => t.CreatedOn).IsRequired();
         entityBuilder.Property(t => t.CreatedBy).IsRequired().HasMaxLength(255);
 
+        entityBuilder.HasIndex(t => new { t.CreatedOn, t.CreatedBy, t.ActionType })
+            .HasDatabaseName("IX_AUD_CRON_CBBY_ACTY");
+        
         entityBuilder.Ignore(x => x.IsDeleted);
         entityBuilder.Ignore(x => x.ModifiedBy);
         entityBuilder.Ignore(x => x.ModifiedOn);
         entityBuilder.Ignore(x => x.RowVersion);
     }
+
 }
