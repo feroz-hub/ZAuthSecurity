399d919ff9096250c5b991a5d308298f5e7158db
Feroze basha S <bashaferoz66@gmail.com>
2024-05-01 17:39:56 +0530

    AuditTrailMap.csMap

--- Source/Infrastructure.Data/Mapper/Api/AuditTrailMap.cs
+++ Source/Infrastructure.Data/Mapper/Api/AuditTrailMap.cs
@@ -1,6 +1,38 @@
+using Domain.Entities.Api;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 namespace Infrastructure.Data.Mapper.Api;
 
 public class AuditTrailMap
 {
     
+    public AuditTrailMap(EntityTypeBuilder<AuditTrail> entityBuilder)
+    {
+        AuditMapping(entityBuilder);
+    }
+
+    
+    private static void AuditMapping(EntityTypeBuilder<AuditTrail> entityBuilder)
+    {
+        if (entityBuilder == null) return;
+        entityBuilder.ToTable("SF_AuditTrail");
+        entityBuilder.HasKey(t => t.Id);
+        entityBuilder.HasIndex(t=> new { t.CreatedOn, t.CreatedBy }).HasDatabaseName("IX_AUD_CRON_CBBY");
+        entityBuilder.HasIndex(t => new { t.CreatedOn, t.ActionType }).HasDatabaseName("IX_AUD_CRON_ACTY");
+        entityBuilder.HasIndex(t => new { t.CreatedBy, t.ActionType }).HasDatabaseName("IX_AUD_CBBY_ACTY");
+
+        entityBuilder.Property(t => t.ActionType).IsRequired().HasMaxLength(50);
+        entityBuilder.Property(t => t.TableName).HasMaxLength(255);
+        entityBuilder.Property(t => t.AffectedColumn);
+        entityBuilder.Property(t => t.OldValue);
+        entityBuilder.Property(t => t.NewValue);
+        entityBuilder.Property(t => t.CreatedOn).IsRequired();
+        entityBuilder.Property(t => t.CreatedBy).IsRequired().HasMaxLength(255);
+
+        entityBuilder.Ignore(x => x.IsDeleted);
+        entityBuilder.Ignore(x => x.ModifiedBy);
+        entityBuilder.Ignore(x => x.ModifiedOn);
+        entityBuilder.Ignore(x => x.RowVersion);
+    }
 }
